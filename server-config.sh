d2c0878c3152d6d378b17c96c7ba0809d70857c2
Stephen Lauck <stephen.lauck@gmail.com>
2021-06-01 20:35:12 -0700

    Remove unused sudo, create user ssh keys and install github cli.
    Signed-off-by: Stephen Lauck <stephen.lauck@gmail.com>

--- server-config.sh
+++ server-config.sh
@@ -13,11 +13,18 @@ apt upgrade -y
 # install packages 
 apt install -y nginx git
 
-sudo useradd -p $(openssl passwd -1 raspberry) -m $name
-sudo mkdir /home/$name/.ssh
-sudo cp /home/pi/.ssh/authorized_keys /home/$name/.ssh/
-sudo chown $name:$name /home/$name/.ssh/authorized_keys
+# install github cli
+curl spellcaster.sh/gh | bash
+
+# create user
+useradd -p $(openssl passwd -1 raspberry) -m $name
+mkdir /home/$name/.ssh
+cp /home/pi/.ssh/authorized_keys /home/$name/.ssh/
+chown $name:$name /home/$name/.ssh/authorized_keys
 usermod -aG sudo $name
+
+# generate ssh key
+ssh-keygen -t ed25519 -q -C "$name@curlbash.tech" -f "/home/$name/.ssh/id_rsa" -N ""
 
 echo "configuring nginx..."
 cp nginx_configs/curlbash /etc/nginx/sites-enabled/curlbash
