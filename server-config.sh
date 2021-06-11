fdcc31717f9adf3df0285681f4fee6e413c38209
Mark M. Florida <mflorida@users.noreply.github.com>
2021-06-10 19:43:00 -0500

    Add user before updating system

--- server-config.sh
+++ server-config.sh
@@ -3,8 +3,13 @@
 #!/bin/bash
 
 name=$1
-echo "Hello, $name"
-echo "Running Configuration..."
+echo "Running configuration..."
+
+echo "Adding user $name"
+useradd -p $(openssl passwd -1 raspberry) -G sudo -m $name 
+mkdir /home/$name/.ssh
+cp /home/pi/.ssh/authorized_keys /home/$name/.ssh/
+chown -R $name:$name /home/$name/.ssh
 
 # update system
 apt update
@@ -12,12 +17,6 @@ apt upgrade -y
 
 # install packages
 apt install git nginx openjdk-11-jdk-headless
-
-echo "adding user $name"
-useradd -p $(openssl passwd -1 raspberry) -G sudo -m $name 
-mkdir /home/$name/.ssh
-cp /home/pi/.ssh/authorized_keys /home/$name/.ssh/
-chown -R $name:$name /home/$name/.ssh
 
 # echo "configuring nginx..."
 # cp nginx_configs/curlbash /etc/nginx/sites-enabled/curlbash
