2c6598e8c09f1c1f4b9882dae09f6160463651b2
Mark M. Florida <mflorida@users.noreply.github.com>
2021-06-18 00:17:09 -0500

    add flags to start Minecraft server in the background

--- server-config.sh
+++ server-config.sh
@@ -27,9 +27,11 @@ if [[ -e server.jar ]]
   else curl https://launcher.mojang.com/v1/objects/1b557e7b033b583cd9f66746b7a9ab1ec1673ced/server.jar -o server.jar
 fi
 echo "eula=true" > eula.txt
-screen java -Xmx1024M -Xms1024M -jar server.jar nogui 
+
+screen -dm java -Xmx1024M -Xms1024M -jar server.jar nogui 
 
 # deploy website
 curl -L  https://github.com/$site/archive/refs/heads/main.zip -o site.zip 
 unzip -o -j site.zip -d curlbash 
 cp -r curlbash /var/www/
+
