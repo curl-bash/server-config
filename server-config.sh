e0b8abc4b123510fb9ad501e352e4830788e9b41
Mark M. Florida <mflorida@users.noreply.github.com>
2021-06-18 02:03:35 -0500

    setup up separate 'minecraft' user
    Add a new user for running the Minecraft server. TODO: start server in the background as 'minecraft' user

--- server-config.sh
+++ server-config.sh
@@ -1,12 +1,12 @@
-# server configuration for raspberry pi
+#!/bin/bash -e
 
-#!/bin/bash
+# server configuration for raspberry pi
 
 name=$1
 echo "Running configuration..."
 
 echo "Adding user $name"
-useradd -p $(openssl passwd -1 raspberry) -G sudo -m $name 
+useradd -p $(openssl passwd -1 rasp) -G sudo -m $name 
 mkdir /home/$name/.ssh
 cp /home/pi/.ssh/authorized_keys /home/$name/.ssh/
 chown -R $name:$name /home/$name/.ssh
@@ -18,9 +18,25 @@ apt upgrade -y
 # install packages
 apt install git nginx openjdk-11-jdk-headless screen
 
-# install minecraft server
+# setup 'minecraft' user to run the Minecraft server
+mcuser="minecraft"
+echo "Adding $mcuser user"
+# does mcuser need to login?
+useradd -p $(openssl passwd -1 rasp) -G sudo -m $mcuser 
+mkdir -p /home/$mcuser
+
+# change to mcuser dir after saving current dir
+CWD=$(pwd)
+cd /home/$mcuser
+
 # download minecraft server if not already downloaded
 [[ ! -e server.jar ]] \
   && curl https://launcher.mojang.com/v1/objects/1b557e7b033b583cd9f66746b7a9ab1ec1673ced/server.jar -o server.jar
 echo "eula=true" > eula.txt
-screen -dm java -Xmx1024M -Xms1024M -jar server.jar nogui 
+
+# make sure mcuser owns their files
+chown -R $mcuser:$mcuser /home/$mcuser
+
+sudo -u $mcuser java -Xmx1024M -Xms1024M -jar server.jar nogui 
+
+cd $CWD
