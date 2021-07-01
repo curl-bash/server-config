b48f699a77e356db1c6010b389a61ee4ab3c6f0d
Stephen Lauck <stephen.lauck@gmail.com>
2021-07-01 16:32:21 -0700

    Update server-config.sh

--- server-config.sh
+++ server-config.sh
@@ -20,7 +20,9 @@ apt install git nginx openjdk-11-jdk-headless screen
 
 # install minecraft server
 # download minecraft server if not already downloaded
-[[ ! -e server.jar ]] \
-  && curl https://launcher.mojang.com/v1/objects/1b557e7b033b583cd9f66746b7a9ab1ec1673ced/server.jar -o server.jar
+if [[ -e server.jar ]]
+  then echo "server.jar exists!"
+  else curl https://launcher.mojang.com/v1/objects/1b557e7b033b583cd9f66746b7a9ab1ec1673ced/server.jar -o server.jar
+fi
 echo "eula=true" > eula.txt
 screen java -Xmx1024M -Xms1024M -jar server.jar nogui 
